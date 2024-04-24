78cb6f6fb8116040dffdcba80ec8a461d8bec5c0
VengateshGV <53400321+vengateshkarthick@users.noreply.github.com>
2024-04-24 19:28:47 +0530

    Update next.config.mjs

--- my-app-booker/next.config.mjs
+++ my-app-booker/next.config.mjs
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     domains: ["avatars.githubuserscontent.com", "lh3.googleusercontent.com"],
   },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  }
   // images: {
   //     remotePatterns: [
   //       {
